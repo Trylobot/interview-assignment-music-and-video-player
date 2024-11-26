2dbb5c4b25bfa4a55f1a0ba5f9693168a0d638c3
Trylobot <trylobot@gmail.com>
2024-11-26 13:34:41 -0600

    youtube iframes for thumbnails

--- server/frontend/src/components/YouTubeThumbnail.tsx
+++ server/frontend/src/components/YouTubeThumbnail.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import YouTube, { YouTubeProps } from 'react-youtube';
 
 type YouTubeThumbnailProps = {
   url: string;
 };
 
 const YouTubeThumbnail: React.FC<YouTubeThumbnailProps> = ({ url }) => {
+  
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };  
+
   const getYouTubeId = (url: string): string | null => {
     const regExp =
       /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
@@ -18,13 +34,20 @@ const YouTubeThumbnail: React.FC<YouTubeThumbnailProps> = ({ url }) => {
     return <div>Invalid YouTube URL</div>;
   }
 
-  const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+  // const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+
+  // return (
+  //   <div>
+  //     <img src={thumbnailUrl} alt="YouTube Thumbnail" className="w-full h-auto rounded-lg shadow-md" />
+  //   </div>
+  // );
 
   return (
     <div>
-      <img src={thumbnailUrl} alt="YouTube Thumbnail" className="w-full h-auto rounded-lg shadow-md" />
+      <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} />
     </div>
   );
+
 };
 
 export default YouTubeThumbnail;
